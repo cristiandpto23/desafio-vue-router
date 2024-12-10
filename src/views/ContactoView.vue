cfc58880ef617a2e2c2f6cbd4a470f8014c31c8a
cristiandpto23 <cristian.web.development@gmail.com>
2024-12-10 19:54:02 -0300

    update contact - adds button disabled

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -8,19 +8,19 @@
                         <form>
                             <div class="mb-3">
                                 <label for="nombre" class="form-label">Nombre</label>
-                                <input type="text" class="form-control" id="nombre" v-model="form.nombre" />
+                                <input type="text" class="form-control" id="nombre" v-model="form.nombre" required />
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" v-model="form.email" />
+                                <input type="email" class="form-control" id="email" v-model="form.email" required />
                             </div>
                             <div class="mb-3">
                                 <label for="mensaje" class="form-label">Mensaje</label>
-                                <textarea class="form-control" id="mensaje" v-model="form.mensaje"></textarea>
+                                <textarea class="form-control" id="mensaje" v-model="form.mensaje" required></textarea>
                             </div>
                             <div class="d-flex justify-content-between">
                                 <router-link to="/" class="btn btn-primary">Volver al inicio</router-link>
-                                <button type="submit" class="btn btn-success">Enviar</button>
+                                <button @click="enviar" type="submit" class="btn btn-success" :disabled="!isFormValid">Enviar</button>
                             </div>
                         </form>
                     </div>
@@ -41,6 +41,21 @@ export default {
                 mensaje: '',
             },
         };
+    },
+    computed: {
+        isFormValid() {
+            return this.form.nombre.trim() !== '' && this.form.email.trim() !== '' && this.form.mensaje.trim() !== '';
+        },
+    },
+    methods: {
+        enviar() {
+            if (this.isFormValid) {
+                alert('Formulario enviado. Te contactaremos próximamente <3');
+                this.form.nombre = '';
+                this.form.email = '';
+                this.form.mensaje = '';
+            }
+        },
     },
 };
 </script>
